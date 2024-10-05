bd1b0acc79f548127461916e3c6e2acd90a78afe
Matias28567 <munozquispematiasp@gmail.com>
2024-10-05 11:05:44 -0500

    CLASE CODERDOJO 02

---
+++ scripts.js
@@ -0,0 +1,5 @@
+document.querySelector('#boton-cambiar').addEventListener('click', function() {
+    document.querySelector('h1').textContent = "Minecraft es un juego de aventura.";
+    document.querySelector('img').classList.add('imagen-destacada');
+  }); 
+  
